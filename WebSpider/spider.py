ee20f4c105fd6881df387d7aba96516c3b8479ae
jonathanlee <jonathan1214@foxmail.com>
2018-12-05 00:39:56 +0800

    'add new webpages and solved the image format problem'

--- WebSpider/spider.py
+++ WebSpider/spider.py
@@ -5,8 +5,10 @@ import re
 
 
 def get_one_page(url):
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.110 Safari/537.36'}
-    response = requests.get(url, headers=headers)
+    '''
+    不用headers 这个网站绝了
+    '''
+    response = requests.get(url)
     if response.status_code == 200:
         return response.text
     return None
@@ -18,17 +20,19 @@ def main(offset, last_page_total=0):
     url = "https://www.fabiaoqing.com/search/search/keyword/%E7%8C%AB%E5%92%8C%E8%80%81%E9%BC%A0/type/bq/page/"+str(offset)+'.html'
     html = get_one_page(url)
     results = re.findall('<img.*?data-original="(.*?)".*?>', html, re.S)
-    for index,result in enumerate(results,1):
+    for index,result in enumerate(results,last_page_total+1):
+        # 直接把index变成总的序号
         print(result)
-        f_name = str(index+last_page_total)+'.gif'
-        print('开始下载第%d张表情' % f_name)
+        f_name = str(index)+result[-4:]
+        print('开始下载第%d张表情包' % index)
         with open(f_name, 'wb') as f:
             f.write(requests.get(result).content)
-        print('第%d张表情下载完成' % f_name)
+        print('第%d张表情包下载完成' % index)
     return last_page_total+len(results)
 
 
 if __name__ == "__main__":
     total = 0
-    for i in range(10):
+    for i in range(12):
         total = main(i+1, last_page_total=total)
+    print('下载完成 共%d张表情包' % total)
